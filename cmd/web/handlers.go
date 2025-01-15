7761f10b64c9be8dedda9b2e7346f585693dadca
Nikita Latushka <nikita@latushka.dev>
2025-01-15 14:15:25 -0500

    render page with html/template package

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "fmt"
+    "html/template"
+    "log"
     "net/http"
     "strconv"
 )
@@ -12,7 +14,20 @@ func home(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    w.Write([]byte("Hello from Snippetbox"))
+    // read template file into 'ts' template set; catch errors
+    ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    // write template set content as the response body 'w'
+    err = ts.Execute(w, nil)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
 }
 
 func snippetView (w http.ResponseWriter, r *http.Request) {
