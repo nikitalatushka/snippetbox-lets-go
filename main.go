8a4b8a0536d1746bcfe2db17ef9aa8a5fbe581f1
Nikita Latushka <nikita@latushka.dev>
2025-01-15 09:58:40 -0500

    register handlers for /snippet/view and /snippet/create

--- main.go
+++ main.go
@@ -5,23 +5,29 @@ import (
     "net/http"  // for http client and server implementations
 )
 
-// create handler function
-// writes a byte slice as the response body
+// create handler functions
+// write byte slice as the response body
 func home(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello from Snippetbox"))
 }
+func snippetView (w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Display a specific snippet"))
+}
+func snippetCreate (w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Create a new snippet"))
+}
 
 func main() {
-    // create router (initialize new servmux)
-    // register handler function for a route "/"
-    // "/" is a catch-all URL pattern in servemux 
-    // servmux will receive HTTP request and check the URL path
-    // then dispatch to the handler function
+    // create servmux router
+    // 'mux' recieves HTTP requests and checks URL path 
     mux := http.NewServeMux()
+    // register handler functions 
+    // 'mux' will dispatch to the handler function of a URL path
     mux.HandleFunc("/", home)
+    mux.HandleFunc("/snippet/view", snippetView)
+    mux.HandleFunc("/snippet/crete", snippetCreate)
 
-    // start web server listening on port 4000
-    // uses mux for routing
+    // start web server listening on port 4000 and routing with 'mux'
     // if server returns an error, log.Fatal will exit server
     log.Println("starting server on :4000")
     err := http.ListenAndServe(":4000", mux)
