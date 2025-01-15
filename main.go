5505947a2329d948fdc8af0acd724ba147e018b3
Nikita Latushka <nikita@latushka.dev>
2025-01-15 12:31:29 -0500

    get ready for refactoring: clean up comments

--- main.go
+++ main.go
@@ -1,55 +1,34 @@
 package main 
 
 import (
-    "fmt"       // for formatting and printing output
-    "log"       // for simple logging
-    "net/http"  // for http client and server implementations
-    "strconv"   // for converting strings
+    "fmt"
+    "log"
+    "net/http"
+    "strconv"
 )
 
-// create handler functions
-// write byte slice as the response body
 func home(w http.ResponseWriter, r *http.Request) {
-    // this handler is used for the "/" url path
-    // "/" is a subtree path which acts as a wildcard
-    // we want the page to be displayed only for the fixed "/" path
-    // users should receive a 404 response otherwise using http.NotFound()
-    // restrict the root URL pattern with this simple check
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
     }
+
     w.Write([]byte("Hello from Snippetbox"))
 }
+
 func snippetView (w http.ResponseWriter, r *http.Request) {
-    // strconv.Atoi() takes a string as input
-    // strconv.Atoi() returns 2 values: an integer and an error
-    // return NotFound if error != nil or id is less than 1
-    // use Golang short variable declaration operator `:=`
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
         http.NotFound(w, r)
         return
     }
+
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
-func snippetCreate (w http.ResponseWriter, r *http.Request) {
-    // this handler will eventually create a new snippet in the database
-    // which is a non-idempotent action that changes server state
-    // so subsequent calls with the same input can yield different results
-    // HTTP good practice is to restrict methods on routes
-    // in this case we want to restrict route to act on POST request only
-    // it will send a 405 Method Not Allowed unless the method is POST
-    // return from create snippet so subsequent code is not executed
-    // test with `$ curl -i -X POST http://localhost:4000/snippet/create`
-    if r.Method != http.MethodPost {
-        
-        // add "Allow: POST" to the response header map
-        w.Header().Set("Allow", http.MethodPost)
 
-        // send status code and plain-text response body
-        // use helper function http.Error()
-        // to replace w.WriteHeader() and w.Write([]byte())
+func snippetCreate (w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        w.Header().Set("Allow", http.MethodPost)
         http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
         return
     }
@@ -58,28 +37,11 @@ func snippetCreate (w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    // create servmux router
-    // 'mux' recieves HTTP requests and checks URL path 
-    // longer URL patterns always take precedence over shorter ones
-    // you can register patterns in any order
-    // request URLs are automatically sanitized
-    // automatic 301 Permanent Redirects for subtree paths and sanitized paths
-    // servmux doesn't support regexp-based patterns
-    // does not support URL variables for RESTFUL api
     mux := http.NewServeMux()
-    // register handler functions 
-    // 'mux' will dispatch to the handler function of a URL path
-    // fixed URL paths don't end with trailing slash "/view"
-    // require exact match to call corresponding handler
-    // subtree URL paths do end with trailing slach "/view/"
-    // call handlers when start of request path matches
-    // like a wild card "/view/" == "/view/**"
     mux.HandleFunc("/", home)
     mux.HandleFunc("/snippet/view", snippetView)
     mux.HandleFunc("/snippet/create", snippetCreate)
-
-    // start web server listening on port 4000 and routing with 'mux'
-    // if server returns an error, log.Fatal will exit server
+    
     log.Println("starting server on :4000")
     err := http.ListenAndServe(":4000", mux)
     log.Fatal(err)
