f00e8b77eca2f2897e3f90a3bf61ffdc69ef79bb
Nikita Latushka <nikita@latushka.dev>
2025-01-17 12:32:08 -0500

    set stack trace frame depth to 2; logger will report filename and linenumber one step back from error to show where it originated from

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -11,7 +11,7 @@ import (
 // sends generic 500 Internal Server Error response to user
 func (app *application) serverError(w http.ResponseWriter, err error) {
     trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-    app.errorLog.Println(trace)
+    app.errorLog.Output(2, trace)
 
     http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
