b06c84074849a859c40258ecea1781c835149385
Nikita Latushka <nikita@latushka.dev>
2025-01-17 11:46:15 -0500

    1/2 inject dependencies by putting them in an application 'struct' then update handler functions so that they become methods against the 'struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,7 @@ import (
     "strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
@@ -25,7 +25,7 @@ func home(w http.ResponseWriter, r *http.Request) {
     // read template files into 'ts' template set; catch errors
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        log.Println(err.Error())
+        app.errorLog.Println(err.Error())
         http.Error(w, "Internal Server Error", 500)
         return
     }
@@ -33,12 +33,12 @@ func home(w http.ResponseWriter, r *http.Request) {
     // write template set content as the response body 'w'
     err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
-        log.Println(err.Error())
+        app.errorLog.Println(err.Error())
         http.Error(w, "Internal Server Error", 500)
     }
 }
 
-func snippetView (w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView (w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
         http.NotFound(w, r)
@@ -48,7 +48,7 @@ func snippetView (w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate (w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate (w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
         w.Header().Set("Allow", http.MethodPost)
         http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
